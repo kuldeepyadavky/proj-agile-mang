b05b604b5a8f9360c0854d52a75730083b921289
Kuldeep Yadav <kuldeepyadav@LA-183.local>
2021-11-11 14:24:13 +0530

    adding validations in frontend

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :task
   belongs_to :user
   validates :body, presence: :true
+  validates_length_of :body, minimum: 5, maximum: 1000
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,6 @@ class Project < ApplicationRecord
   has_many :tasks, dependent: :destroy
   belongs_to :user 
   validates :title, :description, :status,:due_date ,presence: :true
+  validates_length_of :title, minimum: 5, maximum: 255
+  validates_length_of :description, minimum: 5, maximum: 1000
 end
