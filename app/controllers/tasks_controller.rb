5e08675e0cfa067e78603f39bdfeb5a2b532d015
Kuldeep Yadav <kuldeepyadav@LA-183.local>
2021-11-12 05:49:21 +0530

    project show

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -65,6 +65,6 @@ class TasksController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def task_params
-      params.require(:task).permit(:title, :description, :due_date, :end_date, :start_date, :story_point, :status, :task_type,  :project_id, :priority, :user_id, attached_files:[])
+      params.require(:task).permit(:title, :phase_id, :description, :due_date, :end_date, :start_date, :story_point, :status, :task_type,  :project_id, :priority, :user_id, attached_files:[])
     end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -3,7 +3,7 @@ class Task < ApplicationRecord
   has_many :sub_tasks, dependent: :destroy
   belongs_to :project
   belongs_to :user
-  belongs_to :task
+  belongs_to :phase
   validates :title, :description, :due_date, :end_date, :priority, :story_point, :start_date, :status, :task_type, presence: :true
   #file upload
   has_many_attached :attached_files
