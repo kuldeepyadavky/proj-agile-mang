8d11d0d0416d45163bfe3ac721ef4d324825715b
Kuldeep Yadav <kuldeepyadav@LA-183.local>
2021-11-12 14:45:24 +0530

    signup test

--- spec/features/project_spec.rb
+++ spec/features/project_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe 'Projects' do
-    describe 'user interaction with project page' do
-        it 'user who arent logged in should not see the content of project page' do
+    describe 'not a registered user interaction with project page' do
+        it 'Should not see the content of project page' do
             visit('/projects')
             expect(page).to have_no_content('Projects')
         end
     end
+    
 end
