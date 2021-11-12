8260373deb44be5e2882b1ea72593dcaf3662e72
Kuldeep Yadav <kuldeepyadav@LA-183.local>
2021-11-12 12:35:18 +0530

    project_spec feature 1

---
+++ spec/features/project_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe 'Projects' do
+    describe 'user interaction with project page' do
+        it 'user who arent logged in should not see the project page' do
+            visit('/projects')
+            expect(page).to have_no_content('Projects')
+        end
+    end
+end
